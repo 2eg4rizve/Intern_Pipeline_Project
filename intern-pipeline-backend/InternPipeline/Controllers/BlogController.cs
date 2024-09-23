c6fb01d6e898adcf4dd0104a45e8d73a91c36be6
2eg4rizve <2eg4rizve@gmail.com>
2024-09-23 18:03:51 +0600

    only blog create

---
+++ intern-pipeline-backend/InternPipeline/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using InternPipeline.Entities.RequestEntity;
+using InternPipeline.Managers.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternPipeline.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        //hello
+        private readonly IBlogManager _blogManager;
+
+        public BlogController(IBlogManager blogManager)
+        {
+            _blogManager = blogManager;
+        }
+
+        [HttpPost("create-blog")]
+        public async Task<IActionResult> CreateBlog(CreateBlogRequestEntity createBlogRequestEntity)
+        {
+            var response = await _blogManager.CreateBlog(createBlogRequestEntity);
+            return Ok(response);
+        }
+    }
+}
